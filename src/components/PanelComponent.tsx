39e2d2b16530d48672160b787801f12612188320
Mariam <mariam@mixodrink.com>
2023-11-08 11:57:46 +0100

    Update PanelComponent

--- src/components/PanelComponent.tsx
+++ src/components/PanelComponent.tsx
@@ -1,8 +1,10 @@
+
 import styled from 'styled-components';
 
 import alerta from '../assets/png/alertas.png';
 import message from '../assets/icons/message.png';
-import close from '../assets/png/close.png';
+import close from "../assets/png/close.png"
+
 
 const getColor = (cantidad: number) => {
   if (cantidad < 100) {
@@ -14,25 +16,33 @@ const getColor = (cantidad: number) => {
   }
 };
 
-const ListItemData = [
-  { id: 1, item: 'Combinados', price: '2.484.000€', color: 'rgba(255, 122, 0, 0.2)' },
-  { id: 2, item: 'Refrescos', price: '1.266.000€', color: 'rgba(82, 0, 255, 0.2)' },
-  { id: 3, item: 'Agua', price: '643.000€', color: 'rgba(0, 133, 255, 0.2)' },
-];
-
-const List = [
-  { id: 1, item: 'Presion de agua', status: 'Ok', cantidad: 0 },
-  { id: 2, item: 'Temperatura', status: '37ºc', cantidad: 0 },
-  { id: 3, item: 'Vasos', status: '237', cantidad: 237 },
-  { id: 4, item: 'Conexion', status: '178Mbs', cantidad: 0 },
-  { id: 5, item: 'Hielo', status: '2kg', cantidad: 0 },
-  { id: 6, item: 'TPV Conexion', status: 'OK', cantidad: 0 },
-  { id: 4, item: 'Conexion', status: '178Mbs', cantidad: 0 },
-  { id: 5, item: 'Hielo', status: '2kg', cantidad: 0 },
-  { id: 6, item: 'TPV Conexion', status: 'OK', cantidad: 0 },
-];
-
 export const PanelComponent = () => {
+
+  interface ListItemData {
+    id: number;
+    item: string;
+    status: string;
+    cantidad: number;
+  }
+
+  const data = [
+    { id: 1, item: 'Combinados', price: '2.484.000€', color: 'rgba(255, 122, 0, 0.2)' },
+    { id: 2, item: 'Refrescos', price: '1.266.000€', color: 'rgba(82, 0, 255, 0.2)' },
+    { id: 3, item: 'Agua', price: '643.000€', color: 'rgba(0, 133, 255, 0.2)' },
+  ];
+
+  const List: ListItemData[] = [
+    { id: 1, item: 'Presion de agua', status: 'Ok', cantidad: 0 },
+    { id: 2, item: 'Temperatura', status: '37ºc', cantidad: 0 },
+    { id: 3, item: 'Vasos', status: '237', cantidad: 237 },
+    { id: 4, item: 'Conexion', status: '178Mbs', cantidad: 0 },
+    { id: 5, item: 'Hielo', status: '2kg', cantidad: 0 },
+    { id: 6, item: 'TPV Conexion', status: 'OK', cantidad: 0 },
+    { id: 4, item: 'Conexion', status: '178Mbs', cantidad: 0 },
+    { id: 5, item: 'Hielo', status: '2kg', cantidad: 0 },
+    { id: 6, item: 'TPV Conexion', status: 'OK', cantidad: 0 },
+  ];
+
   return (
     <SectionContent>
       <TitlePanel>Control Panel</TitlePanel>
@@ -42,28 +52,34 @@ export const PanelComponent = () => {
             <img src={message} alt="" />
             <span>Informacion</span>
           </InfoIcon>
-          Bienvenido a tu Mixo Dashboard! <br />
-          Esta es la pantalla del Panel de Control donde encontrarás acceso a componentes del
-          salpicadero como facturación, alertas o stock.<br />
-          En el menú lateral puedes encontrar la página con los detalles de cada componente.
+          Bienvenido a tu Mixo Dashboard! <br></br>
+          Esta es la pantalla del Panel de Control donde encontrará acceso Componentes del
+          salpicadero como facturación, alertas o stock.<br></br>
+          En el menú lateral puede encontrar la página con los detalles de cada compon.
           <IconClose>
-            <img src={close} alt="" />
+            <img src={close} alt=''/>
           </IconClose>
         </DescriptionText>
       </DescriptionPanel>
       <SectionPanel>
         <BillingInfo>
           <BillingWrapper>
-            <BillingTitle>Facturación</BillingTitle>
+            <BillingTitle>Facturación</BillingTitle>
             <CalendarContainer>Calendario</CalendarContainer>
           </BillingWrapper>
           <Billings>
-            {ListItemData.map((item) => (
-              <BillingItem key={item.id} color={item.color}>
-                <p className="item-name">{item.item}</p>
-                <p className="item-price">{item.price}</p>
-              </BillingItem>
-            ))}
+            <Combined key={data[0].id} color={data[0].color}>
+              <p>{data[0].item}</p>
+              <p>{data[0].price}</p>
+            </Combined>
+            <Refreshments key={data[1].id} color={data[1].color}>
+              <p>{data[1].item}</p>
+              <p>{data[1].price}</p>
+            </Refreshments>
+            <Water key={data[2].id} color={data[2].color}>
+              <p>{data[2].item}</p>
+              <p>{data[2].price}</p>
+            </Water>
           </Billings>
         </BillingInfo>
         <MaintenianceInfo>
@@ -98,7 +114,6 @@ const SectionContent = styled.section`
   padding: 0 2% 0 19%;
   overflow: hidden;
 `;
-
 const TitlePanel = styled.h1`
   width: 266px;
   height: 46px;
@@ -110,7 +125,6 @@ const TitlePanel = styled.h1`
   align-self: flex-start;
   margin-right: auto;
 `;
-
 const DescriptionPanel = styled.div`
   width: 671px;
   height: 135px;
@@ -122,40 +136,35 @@ const DescriptionPanel = styled.div`
   padding: 10px;
   margin: 10px 0 20px 0;
 `;
-
 const DescriptionText = styled.div`
   font-family: 'Montserrat', sans-serif;
   font-size: 12px;
   text-align: left;
   padding: 10px;
-  line-height: 1.5;
+  line-height: 1, 5;
 `;
-
 const InfoIcon = styled.div`
   display: flex;
   align-items: center;
 
   img {
-    width: 20px;
-    height: 20.06px;
-    margin-right: 8px;
+    width: 24px;
+    height: 24px; 
+    margin-right: 8px; 
   }
-
   span {
     font-size: 16px;
   }
+}
 `;
-
 const IconClose = styled.div`
-  align-self: flex-start;
-  
-  img {
-    width: 14.92px;
-    height: 15.27px;
-    margin-bottom: -30px; 
-  }
-`;
+align-self: flex-start;
 
+img {
+  width: 24px;
+  height: 24px;
+}
+`;
 const SectionPanel = styled.section`
   display: flex;
   gap: 30px;
@@ -171,36 +180,62 @@ const BillingInfo = styled.div`
   border-radius: 10px;
   padding: 0 20px 0 20px;
 `;
-
 const BillingWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   margin: 5px 150px 0 0;
 `;
-
 const BillingTitle = styled.div`
   font-family: 'Montserrat', sans-serif;
   font-size: 25px;
   font-weight: 400;
 `;
-
 const CalendarContainer = styled.div``;
 
 const Billings = styled.div``;
-
-const BillingItem = styled.div`
+const Combined = styled.div`
   width: 316px;
   height: 156px;
   border-radius: 10px;
   background-color: ${(props) => props.color};
   text-align: center;
 
-  p.item-name {
+  p {
     font-size: 20px;
   }
 
-  p.item-price {
+  p:nth-child(2) {
+    font-size: 36px;
+  }
+`;
+const Refreshments = styled.div`
+  width: 316px;
+  height: 156px;
+  border-radius: 10px;
+  background-color: ${(props) => props.color};
+  text-align: center;
+
+  p {
+    font-size: 20px;
+  }
+
+  p:nth-child(2) {
+    font-size: 36px;
+  }
+`;
+const Water = styled.div`
+  width: 316px;
+  height: 156px;
+  border-radius: 10px;
+  background-color: ${(props) => props.color};
+  text-align: center;
+
+  p {
+    font-size: 20px;
+  }
+
+  p:nth-child(2) {
     font-size: 36px;
   }
 `;
@@ -221,11 +256,9 @@ const TitleWrapper = styled.div`
   flex-direction: column;
   align-items: flex-start;
 `;
-
 const ListItemAndAlertsWrapper = styled.div`
   display: flex;
 `;
-
 const ListItemWrapper = styled.div`
   width: 458px;
   height: 584px;
@@ -257,7 +290,6 @@ const ListItem = styled.div<{ cantidad: number }>`
     left: 0;
     margin: 5px;
   }
-
   p {
     font-size: 40px;
     margin: 20px;
@@ -270,9 +302,9 @@ const ListItem = styled.div<{ cantidad: number }>`
 
   p.item {
     font-size: 20px;
+    margin: ;
   }
 `;
-
 const Alerts = styled.div`
   width: 442px;
   height: 584px;
@@ -286,7 +318,6 @@ const Alerts = styled.div`
   border-radius: 10px;
   margin: 20px;
 `;
-
 const AlertImage = styled.img`
   width: 100px;
   height: auto;
@@ -294,7 +325,7 @@ const AlertImage = styled.img`
 
 const AlertText = styled.p`
   color: white;
-  font-size: 18px;
+  font-size: ;
 `;
 
 const MaintenianceTitle = styled.div`
@@ -308,5 +339,3 @@ const SelectorMachine = styled.div`
   font-size: 20px;
   margin: 5px 20px;
 `;
-
-
